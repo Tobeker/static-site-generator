e5367bd7cb874875540c7602ce30856f7763efd8
Tobeker <t.wendenburg@yahoo.de>
2025-03-29 09:19:26 +0100

    change public to doc for github

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import shutil
 import os
+import sys
 from pathlib import Path
 from blocktype import *
 
@@ -14,7 +15,7 @@ def copy_directory(path, destination):
             os.mkdir(l_destination)
             copy_directory(l_path, l_destination)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r") as file:
         markdown = file.read()
@@ -28,35 +29,41 @@ def generate_page(from_path, template_path, dest_path):
 
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html_string)
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
 
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
 
     with open(dest_path, "w") as destination:
         destination.write(template)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(from_path):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
 def main():
+    if len(sys.argv[1]) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
     #alten public ordner löschen, wenn einer existiert
-    if os.path.exists("public/"):
-        shutil.rmtree("public/")
+    if os.path.exists("docs"):
+        shutil.rmtree("docs")
     #neuen public ordner erstellen
-    os.mkdir("public/")
-    destination = "public/"
+    os.mkdir("docs")
+    destination = "docs"
     #static verzeichnis kopieren
     if os.path.exists("static/"):
         path = "static/"
         copy_directory(path, destination)
     else:
         raise Exception("no static/ directory")
-    generate_pages_recursive("./content", "./template.html", "./public")
+    generate_pages_recursive("./content", "./template.html", "./docs", basepath)
 
 main()
